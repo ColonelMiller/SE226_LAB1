57e642bec640f8a020fabf5c49a22cb67bce50f6
Huseyin Alp ERC. <151745220+ColonelMiller@users.noreply.github.com>
2026-02-27 17:10:01 +0300

    Update myFirstLabScript.py

--- myFirstLabScript.py
+++ myFirstLabScript.py
@@ -1,7 +1,7 @@
 
 
-studentName = input("What is your name?")
-print(studentName)
+studentName = input("What is your name?\n")
+print("Hello " + studentName + ".")
 
-studentID = input("What is your Student ID?")
-print(studentID)
+studentID = input("What is your Student ID?\n")
+print("Your ID is " + studentID + ".")
