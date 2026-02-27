cd4b1043474a8e0265f73b315506f8666060d404
Huseyin Alp ERC. <151745220+ColonelMiller@users.noreply.github.com>
2026-02-27 17:23:27 +0300

    Create myFirstLabScript.cpp

---
+++ myFirstLabScript.cpp
@@ -0,0 +1,22 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string name;
+    int studentID;
+    cout << " What is your name?" <<endl;
+    
+    cin >> (name);
+    
+    cout << "Hello " << name << "."<<endl;
+    
+    cout << "What is your Student ID?" <<endl;
+    cin >> (studentID);
+    
+    cout << "Your ID is " << studentID<<".";
+   
+    
+    return 0;
+}
